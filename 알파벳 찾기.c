e93276dd9e7e052cfb1fb613b277bdc55da55c29
Jo minjae <63154034+robot703@users.noreply.github.com>
2022-08-04 18:05:03 +0900

    Add files via upload

--- 알파벳 찾기.c
+++ 알파벳 찾기.c
@@ -1,12 +1,26 @@
-//#include<stdio.h>
-//#include<string.h>
-//
-//int main() {
-//	char s[100]="backjoon";
-//
-//	
-//
-//	char *ptr = strchr(s, 'a');
-//	printf("%c", ptr);
-//
-//}
+#include<stdio.h>
+#include<string.h>
+
+
+int main() {
+
+	char arr[100] = "";
+	char arry[] = "abcdefghijklmnopqrstuvwxyz";
+	int arryy[26] = { 0 };
+
+	gets(arr);
+
+	for (int i = 0; i < 26; i++)
+		arryy[i] = -1;
+
+	for (int i = 0; i < 26; i++) {
+		for (int j = 0; j <strlen(arr); j++) {
+			if (arry[i] == arr[j]&&arryy[i]==-1)
+				arryy[i] = j; 
+		}
+		
+	}
+	for (int j = 0; j < 26; j++)
+		printf("%d ", arryy[j]);
+	 
+}
