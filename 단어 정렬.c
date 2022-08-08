23db843fedd828ca9bea04a655bc33241d574675
Jo minjae <63154034+robot703@users.noreply.github.com>
2022-08-09 00:50:05 +0900

    Add files via upload

---
+++ 단어 정렬.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+
+	int n;
+	char arr[50] = "";
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%s", arr);
+
+
+	}
+}
