085b3ac4e1255c5a522ca3fa4b5a0cc80f002d06
Jo minjae <63154034+robot703@users.noreply.github.com>
2022-08-11 15:11:33 +0900

    Add files via upload

---
+++ 부녀회장이 될테야.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+
+	int T, K, N ;
+	int arr[15][15] = { 0, };
+
+	for (int i = 0; i < 15; i++) arr[0][i] = i;
+
+	for (int i = 1; i < 15; i++) {
+		for (int j = 1; j < 15; j++) {
+			arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+		}
+	}
+
+	scanf("%d", &T);
+
+	for (int i = 0; i < T; i++) {
+		scanf("%d %d", &K, &N);
+		printf("%d\n", arr[K][N]);
+	}
+	return 0;
+}
