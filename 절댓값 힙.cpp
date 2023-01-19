7266d3f08d666187f98c2d31911ab7d8a40d6af5
Jo minjae <63154034+robot703@users.noreply.github.com>
2023-01-19 19:13:06 +0900

    Add files via upload

---
+++ 절댓값 힙.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+#include<queue>
+#include<functional>
+
+using namespace std;
+
+struct comp
+{
+	bool operator()(int a, int b)
+	{
+		if (abs(a) == abs(b))
+			return a > b;
+		else
+			return abs(a) > abs(b);
+	}
+};
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int n,a;
+
+	priority_queue<int, vector<int>, comp>pq;
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		if (a == 0) {
+			if (pq.empty())
+				cout << 0<<"\n";
+			else {
+				cout << pq.top()<<"\n";
+				pq.pop();
+			}
+		}
+		else{
+			pq.push(a);
+		}
+	}
+}
