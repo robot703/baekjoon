815149d1f2d144d8820900cf8e3dde0bd684968e
Jo minjae <63154034+robot703@users.noreply.github.com>
2023-01-20 22:42:52 +0900

    Add files via upload

---
+++ 최소힙.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<queue>
+#include<functional>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+
+	int n, a;
+
+	priority_queue<int, vector<int>, greater<int>>pq;
+
+	cin >> n;
+
+
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+
+		if (a == 0) {
+			if (pq.empty()) cout << "0\n";
+			// queue가 비어있을 경우 0 출력
+			else {
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(a);
+		}
+	}
+	return 0;
+}
