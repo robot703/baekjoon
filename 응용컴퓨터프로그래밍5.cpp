76006563f2bf201ef393c3ea9da3fa1388392d72
Jo minjae <63154034+robot703@users.noreply.github.com>
2023-07-04 23:19:31 +0900

    Add files via upload

---
+++ 응용컴퓨터프로그래밍5.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+
+void Frist_Star(int num);
+void Second_Star(int num);
+int main() {
+
+	int n;
+	printf("1번 2번 삼각형을 골라보세요 :");
+	scanf("%d", &n);
+
+	switch (n)
+	{
+	case 1:
+		Frist_Star(n);
+		break;
+	case 2:
+		Second_Star(n);
+		break;
+	}
+}
+
+void Frist_Star(int num) {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j <= i; j++) {
+			printf("* ");
+		}printf("\n");
+	}
+}
+
+void Second_Star(int num) {
+	for (int i = 0; i < 5; i++) {
+		for (int j = i; j < 5; j++) {
+			printf("* ");
+		}printf("\n");
+	}
+}
